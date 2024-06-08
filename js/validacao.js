55717aaa4e344010ab22bc78896f4cfa439d81f7
djxlma <djalmamoreirafilho@gmail.com>
2024-06-07 22:48:54 -0300

    atualização textos

--- js/validacao.js
+++ js/validacao.js
@@ -24,17 +24,13 @@ formsRegitrado.addEventListener('submit', function(event) {
 });
 
 formLogin.addEventListener('submit', function(event) {
-    // Previne o envio padrão do formulário
     event.preventDefault();
 
-    // Obtém os valores dos campos do formulário de login
     const email = document.getElementById('loginEmail').value;
     const password = document.getElementById('loginSenha').value;
 
-    // Verifica se o email inserido está na lista de emails cadastrados
     const emailIndex = emailCadastrado.indexOf(email);
 
-    // Verifica se o índice do email é válido
     if (emailIndex !== -1 && senhaCadastrada[emailIndex] === password) {
         alert('Login bem-sucedido!');
         formLogin.reset();
@@ -43,5 +39,4 @@ formLogin.addEventListener('submit', function(event) {
     } else if (emailIndex === -1){
         alert('Este email não foi cadastrado');
     }
-    // Reseta o formulário de login
 });
